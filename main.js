962f32c4bfb29ec85547659d4369527da59ca06b
Shamus Birch <th35h4y@gmail.com>
2020-07-16 10:04:08 +0000

    minor edits

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-var xhr = new XMLHttpRequest();
+function getData(cb) { 
+    var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api");
-xhr.send();
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.satus == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.satus == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+getData(function(data) {
+    console.log(data);
+});
 
