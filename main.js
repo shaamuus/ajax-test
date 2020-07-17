a80a75b57891994ca1b1aef8dbaafbe2f68c9c5e
Shamus Birch <th35h4y@gmail.com>
2020-07-17 15:14:15 +0000

    updated lesson source code

--- main.js
+++ main.js
@@ -14,7 +14,14 @@ function getData(type, cb) {
 }
 
 function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
     getData(type, function(data) {
-        document.getElementById("data").innerHTML = data;
+        data = data.results;
+
+        data.forEach(function(item) {
+            el.innerHTML += "<p>" + item.name + "</p>";
+        });
     });
 }
