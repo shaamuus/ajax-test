0cbdf2d88dafc05d824665e3ed7d8d3e2915a1d2
Shamus Birch <th35h4y@gmail.com>
2020-07-17 14:46:51 +0000

    added course source code

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-function getData(cb) { 
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-    xhr.send();
-
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.satus == 200) {
+        if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
-getData(function(data) {
-    console.log(data);
-});
-
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
